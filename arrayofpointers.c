7c8a4614552548f8167f972b8ca2eb251528027c
bharath <6301663928b@gmail.com>
2021-05-27 09:27:53 +0530

    array of pointers

---
+++ arrayofpointers.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<conio.h>
+struct student
+{
+    char name[50];
+    int roll;
+    char dob[10];
+    int classs;
+    char sec[5];
+    char village[20];
+};
+int no_of(){
+    int a;
+    printf("enter the no of students=");
+    scanf("%d",&a);
+    return a;
+}
+int names(int a)
+{
+    struct student b[100];
+    for(int i=0;i<a;i++)
+    {
+        printf("enter the student name:");
+        scanf("%s",b[i].name);
+    }
+}
+int roll(int a)
+{
+    struct student b[100];
+    for(int i=0;i<a;i++)
+    {
+        printf("enter the student rollno:");
+        scanf("%d",&b[i].roll);
+    }
+
+}
+int dob(int a)
+{
+    struct student b[100];
+    for(int i=0;i<a;i++)
+    {
+        printf("enter the student dob(d-m-y):");
+        scanf("%s",b[i].dob);
+    }
+
+}
+int lasss(int a)
+{
+    struct student b[100];
+    for(int i=0;i<a;i++)
+    {
+        printf("enter the student class(1-10):");
+        scanf("%d",&b[i].classs);
+    }
+
+}
+int sec(int a)
+{
+    struct student b[100];
+    for(int i=0;i<a;i++)
+    {
+        printf("enter the student sec:");
+        scanf("%s",b[i].sec);
+    }
+
+}
+int village(int a)
+{
+    struct student b[100];
+    for(int i=0;i<a;i++)
+    {
+        printf("enter the student village:");
+        scanf("%s",b[i].village);
+    }
+
+}
+
+int order(int a)
+{
+    struct student b[100];
+
+printf("==============================================================================================\n");
+printf("||name          ||rollno  || dob  || class  ||sec  ||village          ||\n");
+printf("==============================================================================================\n");
+
+    for(int i=0;i<a;i++){
+printf("\n|%s|%d\t|%s\t|%d\t|%s\t|%s",b[i].name,b[i].roll,b[i].dob,b[i].classs,b[i].sec,b[i].village);
+    }
+}
+
+
+int main()
+{
+int a,b;
+a=no_of();
+names(a);
+roll(a);
+dob(a);
+lasss(a);
+sec(a);
+village(a);
+order(a);
+return 0;
+}
+
